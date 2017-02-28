b0407728e227dab370614cbb3b91334c1b79f5b0
Enrique199413 <enrique199413@gmail.com>
2017-02-28 00:14:31 -0600

    https://github.com/BlickLabs/alegpaez/issues/44 consider update with principal many changes of styles

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/lib/css/bootstrap.min.css' ?>">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
 <?php wp_head(); ?>
 
@@ -26,15 +27,47 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<script>
+	function slide() {
+		if (document.querySelector('#hamburgerMenu').classList.contains('showMenu')) {
+			document.querySelector('#hamburgerMenu').classList.add('slideOutRight');
+			document.querySelector('#hamburgerMenu').classList.remove('slideInRight');
+			setTimeout(function () {
+				document.querySelector('#hamburgerMenuIcon').classList.add('hiddeOnWeb');
+				document.querySelector('#imgMenu').classList.add('hiddeOnWeb');
+				document.querySelector('#principalHeader').classList.remove('hiddeInMobil');
+				document.querySelector('#hamburgerMenu').classList.remove('showMenu');
+				document.querySelector('#bars').classList.remove('hiddeOnMobil');
+				document.querySelector('#hamburgerMenu').classList.add('hiddeOnMobil');
+				document.querySelector('#logo').classList.remove('hiddeOnMobil');
+			}, 500);
+		} else {
+			document.querySelector('#hamburgerMenu').classList.add('slideInRight');
+			document.querySelector('#hamburgerMenu').classList.add('showMenu');
+			document.querySelector('#hamburgerMenu').classList.remove('slideOutRight');
+			document.querySelector('#hamburgerMenuIcon').classList.remove('hiddeOnWeb');
+			document.querySelector('#hamburgerMenu').classList.remove('hiddeOnWeb');
+			document.querySelector('#hamburgerMenu').classList.remove('hiddeOnMobil');
+			document.querySelector('#imgMenu').classList.remove('hiddeOnWeb');
+			document.querySelector('#bars').classList.add('hiddeOnMobil');
+			document.querySelector('#logo').classList.add('hiddeOnMobil');
+		}
+	}
+	</script>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'shoreditch' ); ?></a>
-	<header class="header-navbar">
+	<header class="header-navbar" id="principalHeader">
 	  <input type="checkbox" id="navbar-trigger" />
-	  <label class="fa fa-bars" for="navbar-trigger"></label>
-		<a href="http://dev.alegpaez.com/" class="centerLogo">
+	  <label class="fa fa-bars" id="bars" for="navbar-trigger" onclick="slide()"></label>
+		<a href="http://dev.alegpaez.com/" class="centerLogo" id="logo">
 	    <img class="header-navbar-logo" src="<?php echo get_template_directory_uri() . '/img/icons/logo_2.png' ?>"/>
 	  </a>
-	  <ul class="header-navbar-list">
+	  <ul class="header-navbar-list animated hiddeOnMobil" id="hamburgerMenu">
+			<li class="hamburgerMenuIcon hiddeOnWeb" id="hamburgerMenuIcon">
+				<a class="" onclick="slide()">
+						<span class="sr-only">Toggle navigation</span><i class="fa fa-bars"></i>
+				</a>
+			</li>
 			<li class="header-navbar-list-item" hcolor="white">
 		    <a class="header-navbar-list-item-link" href="http://dev.alegpaez.com">
 	        Inicio
@@ -70,6 +103,9 @@
 	        Contacto
 	      </a>
 	    </li>
+			<li class="hiddeOnWeb" id="imgMenu">
+				<img class="img-blog-menu" src="<?php echo get_template_directory_uri() . '/img/icons/logo_2.png' ?>">
+			</li>
 	  </ul>
 
 	</header>
