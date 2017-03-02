23ef07cd61c24a53f92db7a509b7640a48baee92
Enrique199413 <enrique199413@gmail.com>
2017-03-02 09:16:59 -0600

    add jquery resolution

--- header.php
+++ header.php
@@ -27,7 +27,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<script
+	  src="https://code.jquery.com/jquery-1.12.4.min.js"
+	  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+	  crossorigin="anonymous"></script>
 	<script>
+
 	$(document).ready(function() {
 		$("body").click(function(event) {
 			if (event.target.tagName === 'I') {
